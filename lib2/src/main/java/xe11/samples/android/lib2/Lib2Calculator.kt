55a05106e13041bc16d659a536ea6242ee2840b5
xe11 <alexei.laban@gmail.com>
2023-05-16 01:42:06 +0300

    add dependcy on :sample-android-artifact-publishing-lib1:0.0.5 artifact on jitpack.io

--- lib2/src/main/java/xe11/samples/android/lib2/Lib2Calculator.kt
+++ lib2/src/main/java/xe11/samples/android/lib2/Lib2Calculator.kt
@@ -1,6 +1,8 @@
 package xe11.samples.android.lib2
 
+import xe11.samples.android.lib1.Lib1Calculator
+
 class Lib2Calculator {
 
-    fun sum(a: Int, b: Int): Int = a + b
+    fun sum(a: Int, b: Int): Int = Lib1Calculator().sum(a, b)
 }
